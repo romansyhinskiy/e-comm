15e1b989f6ece8f2a5ca44327cf2b340b9904e4f
romansyhinskiy <romansyhinskiy@gmail.com>
2020-08-16 22:45:11 +0300

    sec commit

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -1,30 +1,19 @@
 $(document).ready(function () {
     $('.JS-Open').click(function(){
-        $('.openProposal_Light').addClass('open')
+        $('.openProposal_Light').addClass('open').siblings().removeClass('open')
     })
     $('.JS-Open-Basic').click(function(){
-        $('.openProposal_Basic').addClass('open')
+        $('.openProposal_Basic').addClass('open').siblings().removeClass('open')
     })
     $('.JS-Open-Full').click(function(){
-        $('.openProposal_Full').addClass('open')
+        $('.openProposal_Full').addClass('open').siblings().removeClass('open')
     })
     $('.JS-Open-Premium').click(function(){
-        $('.openProposal_Premium').addClass('open')
+        $('.openProposal_Premium').addClass('open').siblings().removeClass('open')
     })
-    
     
     $('.closeClass').click(function(){
         $(this).parents('.JS-close').find('.open').removeClass('open');
     })
 
-    $('.JS-compareId').click(function(){
-        let id = $(this).data('id');
-        let proposal = $('.openProposal').each(function(){
-            $(this).data()
-        });
-        if(id == proposal){
-            console.log('proposal')
-        }
-    })
-    
 });
