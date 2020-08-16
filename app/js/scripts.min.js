e54120d2806720f6af4e3dc60e5ff6ceab8c5a22
romansyhinskiy <romansyhinskiy@gmail.com>
2020-08-16 21:23:00 +0300

    sec commit

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -1,1 +1,30 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+    $('.JS-Open').click(function(){
+        $('.openProposal_Light').addClass('open')
+    })
+    $('.JS-Open-Basic').click(function(){
+        $('.openProposal_Basic').addClass('open')
+    })
+    $('.JS-Open-Full').click(function(){
+        $('.openProposal_Full').addClass('open')
+    })
+    $('.JS-Open-Premium').click(function(){
+        $('.openProposal_Premium').addClass('open')
+    })
+    
+    
+    $('.closeClass').click(function(){
+        $(this).parents('.JS-close').find('.open').removeClass('open');
+    })
+
+    $('.JS-compareId').click(function(){
+        let id = $(this).data('id');
+        let proposal = $('.openProposal').each(function(){
+            $(this).data()
+        });
+        if(id == proposal){
+            console.log('proposal')
+        }
+    })
+    
+});
