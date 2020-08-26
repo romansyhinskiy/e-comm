8383cb7a72b3692211d32adfce01a5b6fbfa4d31
romansyhinskiy <romansyhinskiy@gmail.com>
2020-08-26 19:50:25 +0300

    sec commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
     $('.JS-Open').click(function(){
-        $('.openProposal_Light').addClass('open').siblings().removeClass('open')
+        $('.openProposal_Light').addClass('open').siblings().removeClass('open');
+        $('body').addClass('overflowed')
     })
     $('.JS-Open-Basic').click(function(){
-        $('.openProposal_Basic').addClass('open').siblings().removeClass('open')
+        $('.openProposal_Basic').addClass('open').siblings().removeClass('open');
+        $('body').addClass('overflowed')
     })
     $('.JS-Open-Full').click(function(){
-        $('.openProposal_Full').addClass('open').siblings().removeClass('open')
+        $('.openProposal_Full').addClass('open').siblings().removeClass('open');
+        $('body').addClass('overflowed')
     })
     $('.JS-Open-Premium').click(function(){
-        $('.openProposal_Premium').addClass('open').siblings().removeClass('open')
+        $('.openProposal_Premium').addClass('open').siblings().removeClass('open');
+        $('body').addClass('overflowed')
     })
     
     $('.closeClass').click(function(){
         $(this).parents('.JS-close').find('.open').removeClass('open');
+        $('body').removeClass('overflowed')
+
     })
 
 });
